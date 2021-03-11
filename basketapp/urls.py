1be6c16bdb376c151165b541a0e6a0099cdea4ef
Mary <ulanovamary@yandex.ru>
2021-03-11 14:44:56 +0300

    5les ht files added

---
+++ basketapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from basketapp.views import basket_add, basket_delete
+
+app_name = 'basketapp'
+
+urlpatterns = [
+    path('basket_add/<int:product_id>/', basket_add, name='basket_add'),
+    path('basket_delete/<int:id>/', basket_delete, name='basket_delete'),
+
+]
---
+++ basketapp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect
+from mainapp.models import Product
+from basketapp.models import Basket
+from django.urls import reverse
+
+
+def basket_add(request, product_id=None):
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user = request.user, product=product)
+
+    if not baskets.exists():
+        basket = Basket(user=request.user, product=product)
+        basket.quantity = 1
+        basket.save()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def basket_delete(request, id=None):
+    basket = Basket.objects.get(id=id)
+    basket.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
